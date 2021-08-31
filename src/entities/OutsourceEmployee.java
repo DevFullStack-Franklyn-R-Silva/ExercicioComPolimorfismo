d5769b6dcc1d2c427b45d4477ebeb23eddb4f258
Franklyn Roberto <franklyn1998@live.com>
2021-08-31 16:38:55 -0300

    ExercicioComPolimorfismo

---
+++ src/entities/OutsourceEmployee.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class OutsourceEmployee extends Employee {
+
+	private Double additionalCharge;
+
+	public OutsourceEmployee() {
+		super();
+	}
+
+	public OutsourceEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+
+	public Double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(Double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+
+	// Methods
+	@Override
+	public double payment() {
+		return super.payment() + additionalCharge * 1.1;
+	}
+
+}
